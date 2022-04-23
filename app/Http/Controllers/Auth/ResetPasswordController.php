4170b0c73d2880e9173c1c05927a5c7be9db98a7
FilipeSantos <24841572+santosfm@users.noreply.github.com>
2022-04-23 18:00:16 +0100

    Update ResetPasswordController.php
    Enabled strong password when user changes the password

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -6,6 +6,12 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\ResetsPasswords;
 
+//use App\Rules\StrongPasswordReset;
+use Illuminate\Support\Facades\Validator;
+//use Illuminate\Support\Facades\Password;
+use Illuminate\Validation\Rules\Password;
+
+
 class ResetPasswordController extends Controller
 {
     /*
@@ -26,5 +32,18 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
+    
+
+    protected function rules()
+    {
+        return [
+            'password' => ['required', 'string', Password::min(8)->numbers()->mixedCase()->letters()->symbols()->uncompromised()]
+            //'password_confirmation' => ['required', 'string', 'confirmed', Password::min(8)->numbers()->mixedCase()->letters()->symbols()->uncompromised()]
+           // 'password' => 'required|string|min:8', //->numbers()->mixedCase()->letters()->symbols()->uncompromised()],
+            //'password_confirmation' => 'required|string|confirmed|min:8' //->numbers()->mixedCase()->letters()->symbols()->uncompromised()]
+        ];
+    }
+
     protected $redirectTo = RouteServiceProvider::HOME;
+
 }
